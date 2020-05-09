b040f5d0a68e0b4f1918f92cca2b6c8634e34e18
mikalburr <60679511+mikalburr@users.noreply.github.com>
2020-05-09 15:44:02 -0500

    Add files via upload

---
+++ cas.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+/**
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+**/
+
+class Solution {
+public:
+    std::string countAndSay(int n) {
+        return "ok";
+    }
+};
+
+int main (){
+    Solution answer = new Solution;
+    std::string output = answer::countAndSay(12);
+
+    std::cout << output;
+}
