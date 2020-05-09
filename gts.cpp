a9fcf47a18ef0ee2489d2d15aeaef1783b68bd07
mikalburr <60679511+mikalburr@users.noreply.github.com>
2020-05-09 15:52:16 -0500

    Add files via upload

---
+++ gts.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/**
+ * 
+ * Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+ * The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.  
+ * 
+ **/
+
+class Solution {
+public:
+    string generateTheString(int n) {
+        
+    }
+};
+
+int main(){
+    
+}
